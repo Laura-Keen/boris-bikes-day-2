408a407f8dba73524bcefb739ae67f7f1a3f0de7
Laura <keenlaura@hotmail.co.uk>
2021-02-04 16:53:05 +0000

    Completed step 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,25 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+
   # def initialize
+  #   @bike = false
   # end
+
+  def dock(bike)
+    @bike = bike
+  end
 
   def release_bike
     Bike.new
+  end
+
+  def check_bike
+    if @bike == false
+      return false
+    else
+      return true
+    end
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
@@ -7,4 +8,21 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it 'returns bike to docking station' do
+    subject.instance_variable_set :@bike, false
+    subject.dock(Bike.new)
+    expect(subject.bike).to be_a(Bike)
+  end
+
+  it 'sees docked bikes' do
+    subject.instance_variable_set :@bike, Bike.new
+    expect(subject.check_bike).to be true
+  end
+
+  it 'doesnt see docked bikes' do
+    subject.instance_variable_set :@bike, false
+    expect(subject.check_bike).to be false
+  end
+
 end
